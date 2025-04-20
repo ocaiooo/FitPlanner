ffe9ffab23bff99044baedef18172f3ae4bbeb5f
CaioCeesaar <caio2001lc@gmail.com>
2025-04-19 23:55:26 -0300

    Update AuthenticatedUserAttribute.cs

--- src/Backend/FitPlanner.Api/Attributes/AuthenticatedUserAttribute.cs
+++ src/Backend/FitPlanner.Api/Attributes/AuthenticatedUserAttribute.cs
@@ -1,12 +1,5 @@
 ﻿using FitPlanner.Api.Filters;
-using FitPlanner.Communication.Responses;
-using FitPlanner.Domain.Respositories.User;
-using FitPlanner.Domain.Security.Tokens;
-using FitPlanner.Exceptions;
-using FitPlanner.Exceptions.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.IdentityModel.Tokens;
 
 namespace FitPlanner.Api.Attributes;
 
