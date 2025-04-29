9b6860861959261723c3be58b482e53c1d751271
CaioCeesaar <caio2001lc@gmail.com>
2025-04-28 23:55:48 -0300

    add some tests

---
+++ tests/CommonTestUtilities/Requests/RequestChangeUserPasswordJsonBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using FitPlanner.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestChangeUserPasswordJsonBuilder
+{
+    public static RequestChangeUserPasswordJson Build(int passwordLength = 10)
+    {
+        return new Faker<RequestChangeUserPasswordJson>()
+            .RuleFor(u => u.Password, f => f.Internet.Password())
+            .RuleFor(u => u.NewPassword, f => f.Internet.Password(passwordLength));
+    }
+}
---
+++ tests/UseCases.Test/User/ChangePassword/ChangePasswordUseCaseTest.cs
@@ -0,0 +1,22 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Requests;
+
+namespace UseCases.Test.User.ChangePassword;
+
+public class ChangePasswordUseCaseTest
+{
+    [Fact]
+    private async Task Success()
+    {
+        var (user, password) = UserBuilder.Build();
+
+        var request = RequestChangeUserPasswordJsonBuilder.Build();
+        request.Password = password;
+        // Todo: createUseCase method
+        var useCase = CreateUseCase(user);
+        
+        Func<Task> act = async () => await useCase.Execute(request);
+
+        
+    }
+}
